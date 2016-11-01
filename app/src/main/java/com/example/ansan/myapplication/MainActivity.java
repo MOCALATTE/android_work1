f74bbf78cbf291b70fc4cd4c45db9a3435a3eec8
MOCALATTE <08dbwjd@naver.com>
2016-11-01 11:57:14 +0900

    SD 카드 파일 저장/삭제

--- app/src/main/java/com/example/ansan/myapplication/MainActivity.java
+++ app/src/main/java/com/example/ansan/myapplication/MainActivity.java
@@ -14,6 +14,10 @@ import android.widget.Switch;
 import android.widget.Toast;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
     String TAG="[SDCard]";//검색용도로 이름표 붙여줄려고 //TAG로 찾음
@@ -30,27 +34,45 @@ public class MainActivity extends AppCompatActivity {
         }
         //sd카드는 핸드폰마다 경로가 다름
         String path= Environment.getExternalStorageDirectory().getAbsolutePath();
-
+        String filename = path + "/myPics" + "/test.txt";//파일이름
         switch (v.getId()) {
-            case R.id.button:
+            case R.id.button://폴더 생성
                 File file = new File(path+"/myPics");
                 file.mkdir();
                 Toast.makeText(getApplicationContext(),"myPics 폴더 생성", Toast.LENGTH_SHORT).show();
                 break;
-            case R.id.button2:
+            case R.id.button2://폴더 삭제
                 File file2 = new File(path+"/myPics");
                 file2.delete();
                 Toast.makeText(getApplicationContext(),"myPics 폴더 삭제", Toast.LENGTH_SHORT).show();
                 break;
-            case R.id.button3:
-                File file3 = new File(path+"/myPics");
-                file3.mkdir();
-                Toast.makeText(getApplicationContext(),"myPics 파일 생성", Toast.LENGTH_SHORT).show();
+            case R.id.button3://파일 생성
+
+                String content = "안녕하세요";
+                try {
+                    FileOutputStream fos = new FileOutputStream(filename);
+                    fos.write(content.getBytes());
+                    fos.close();
+                    Toast.makeText(getApplicationContext(),"파일 생성", Toast.LENGTH_SHORT).show();
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                    Toast.makeText(getApplicationContext(),"파일 생성 에러", Toast.LENGTH_SHORT).show();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
                 break;
-            case R.id.button4:
-                File file4 = new File(path+"/myPics");
-                file4.delete();
-                Toast.makeText(getApplicationContext(),"myPics 파일 삭제", Toast.LENGTH_SHORT).show();
+            case R.id.button4://파일 삭제
+                try {
+                    FileInputStream fos = new FileInputStream(filename);
+                    byte arr[] = new byte[fos.available()];
+                    fos.close();
+                    Toast.makeText(getApplicationContext(),new String(arr), Toast.LENGTH_SHORT).show();
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                    Toast.makeText(getApplicationContext(),"파일 삭제 에러", Toast.LENGTH_SHORT).show();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
                 break;
         }
     }
